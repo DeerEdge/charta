2aacce024f2983cfe1a3a068a19a81813406b36e
Dheeraj Vislawath <57468596+DeerEdge@users.noreply.github.com>
2024-12-20 23:52:43 -0700

    strategy text area

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 import dash_bootstrap_components as dbc
 import plotly.express as px
 from websockets.asyncio.async_timeout import timeout
+from backtesting import *
 
 from dataloader import *
 from dash import dash_table
@@ -75,61 +76,82 @@ def render_content(tab):
         # Period options: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max
         return html.Div([
             html.Div([
-                dcc.Input(id="symbol_field", type="text", placeholder="Enter Symbol"),
-                html.Div(id="ticker_label", style={'display': 'inline-block', 'margin-left': '10px'})
-            ], style={'margin-bottom': '20px'}),
+                html.Div([
+                    html.Div(id="ticker_label", style={'display': 'inline-block', 'margin-left': '10px'})
+                ], style={'margin-bottom': '20px'}),
+
+                html.Div([
+                    dcc.RadioItems(
+                        id='period-selector',
+                        options=[
+                            {'label': '1D', 'value': '1d'},
+                            {'label': '5D', 'value': '5d'},
+                            {'label': '1M', 'value': '1mo'},
+                            {'label': '3M', 'value': '3mo'},
+                            {'label': '6M', 'value': '6mo'},
+                            {'label': '1Y', 'value': '1y'},
+                            {'label': 'YTD', 'value': 'ytd'},
+                            {'label': 'MAX', 'value': 'max'}
+                        ],
+                        value='max',
+                        inline=True,
+                        style={
+                            'display': 'flex',
+                            'flexWrap': 'wrap',
+                            'gap': '10px'
+                        },
+                        inputStyle={'display': 'none'},
+                        labelStyle={
+                            'display': 'inline-block',
+                            'padding': '10px 15px',
+                            'backgroundColor': '#1f2539',
+                            'color': '#ffffff',
+                            'border': '1px solid #7296f7',
+                            'cursor': 'pointer',
+                            'textAlign': 'center'
+                        },
+                        className='period-selector'
+                    )
+                ], style={'marginBottom': '20px'}),
+
+                dcc.Graph(
+                    id="candlestick_chart",
+                    figure=go.Figure(data=[go.Candlestick()]).update_layout(
+                        title="Enter a ticker symbol",
+                        xaxis_title="Date",
+                        yaxis_title="Price",
+                        xaxis_rangeslider_visible=True,
+                        template="plotly_dark",
+                        height=600,
+                        width=800
+                    )
+                ),
+
+                html.Div(id="num_rows_label"),
+                html.Div(id="table_container")  # DataTable
+
+            ], style={'width': '60%', 'display': 'inline-block', 'vertical-align': 'top'}),
 
             html.Div([
-                dcc.RadioItems(
-                    id='period-selector',
-                    options=[
-                        {'label': '1D', 'value': '1d'},
-                        {'label': '5D', 'value': '5d'},
-                        {'label': '1M', 'value': '1mo'},
-                        {'label': '3M', 'value': '3mo'},
-                        {'label': '6M', 'value': '6mo'},
-                        {'label': '1Y', 'value': '1y'},
-                        {'label': 'YTD', 'value': 'ytd'},
-                        {'label': 'MAX', 'value': 'max'}
-                    ],
-                    value='max',
-                    inline=True,
+                html.Div([
+                    dcc.Input(id="symbol_field", type="text", placeholder="Enter Symbol"),
+                ], style={'margin-bottom': '20px'}),
+                dcc.Textarea(
+                    id='strategy-input',
+                    placeholder='Enter your trading strategy code here...',
                     style={
-                        'display': 'flex',
-                        'flexWrap': 'wrap',
-                        'gap': '10px'
-                    },
-                    inputStyle={'display': 'none'},
-                    labelStyle={
-                        'display': 'inline-block',
-                        'padding': '10px 15px',
+                        'width': '100%',
+                        'height': 600,
                         'backgroundColor': '#1f2539',
                         'color': '#ffffff',
                         'border': '1px solid #7296f7',
-                        'cursor': 'pointer',
-                        'textAlign': 'center'
-                    },
-                    className='period-selector'
+                        'borderRadius': '5px',
+                        'padding': '10px'
+                    }
                 )
-            ], style={'marginBottom': '20px'}),
-
-            dcc.Graph(
-                id="candlestick_chart",
-                figure=go.Figure(data=[go.Candlestick()]).update_layout(
-                    title="Enter a ticker symbol",
-                    xaxis_title="Date",
-                    yaxis_title="Price",
-                    xaxis_rangeslider_visible=True,
-                    template="plotly_dark",
-                    height=600,
-                    width=800
-                )
-            ),
-            
-            html.Div(id="num_rows_label"),
-            html.Div(id="table_container")  # DataTable
-
-        ])
+            ], style={'width': '40%', 'display': 'inline-block', 'vertical-align': 'top', 'marginLeft': '0%',
+                      'marginRight': '2%', 'marginTop': '4%',})
+        ], style={'display': 'flex', 'width': '100%'})
 
     elif tab == 'learn':
         return html.Div([
